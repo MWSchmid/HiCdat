d091d9f5fc2c87cfb2ae401fe6c1564aad7f42ec
MWSchmid <marcschmid@gmx.ch>
2015-01-27 11:38:30 +0100

    renaming program

--- source/HiCdat/macMain.cpp
+++ source/HiCdat/macMain.cpp
@@ -1,12 +1,12 @@
 /*
-This file is part of HiCat.
+This file is part of HiCdat.
 
-HiCat is free software: you can redistribute it and/or modify
+HiCdat is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-HiCat is distributed in the hope that it will be useful,
+HiCdat is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
